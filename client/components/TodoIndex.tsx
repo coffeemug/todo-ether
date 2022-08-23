d0752c3521f2fb775af820949b7be95e86fca5e6
Vyacheslav Akhmechet <coffeemug@gmail.com>
2022-08-23 04:18:13 -0700

    add task dialog

--- client/components/TodoIndex.tsx
+++ client/components/TodoIndex.tsx
@@ -1,13 +1,15 @@
-import { useContractRead, useProvider } from 'wagmi'
+import { useContractRead } from 'wagmi'
 import TasklistContract from '../../hardhat/artifacts/contracts/Tasklist.sol/Tasklist.json';
 import { Interface } from "ethers/lib/utils"
-import { BigNumber, ethers } from 'ethers';
-import { useEffect } from 'react';
+import { BigNumber } from 'ethers';
+import { AddTask } from './AddTask'
+import { useState } from 'react';
 
 const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const abi = new Interface(TasklistContract.abi);
 
 export const TodoIndex = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const { isLoading, isError, data } = useContractRead({
     addressOrName: address,
     contractInterface: TasklistContract.abi,
@@ -19,7 +21,7 @@ export const TodoIndex = () => {
   if (isError) {
     return <p>Something went wrong</p>;
   } else if (isLoading) {
-    return <p>Loading...</p>;
+    return <p className="-mt-24 text-gray-500 text-xl">Loading...</p>;
   }
 
   const taskCount = (data as unknown as BigNumber).toNumber();
@@ -27,7 +29,11 @@ export const TodoIndex = () => {
   if (taskCount === 0) {
     return (
       <div>
-        <h2 className="-mt-16 text-gray-500 text-xl">No tasks yet. <a href="#" className='text-blue-500'>Create one</a>!</h2>
+        <h2 className="-mt-24 text-gray-500 text-xl">No tasks yet. <a href="#" onClick={() => setShowAddTask(true)} className='text-blue-500'>Create one</a>!</h2>
+        <AddTask
+          open={showAddTask}
+          setOpen={setShowAddTask}
+        />
       </div>
     )
   } else {
