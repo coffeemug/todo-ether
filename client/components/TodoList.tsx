dd82b3de074f54cbb80b3120d1e6266efdcc3d04
Vyacheslav Akhmechet <coffeemug@gmail.com>
2022-08-24 15:46:30 -0700

    submissions now work (but dont update automagically yet). Also need to be able to (a) list them, and (b) pcik one.

--- client/components/TodoList.tsx
+++ client/components/TodoList.tsx
@@ -4,13 +4,21 @@ import { range } from 'lodash';
 import { TasklistContract, address } from "../lib/shared";
 import { Loading } from './Loading';
 import { AddTask } from './AddTask'
+import { AddJob } from './AddJob';
 
 type TodoListProps = {
   nTasks: number,
 }
 
+type TaskInfo = {
+  taskIdx?: number | undefined,
+  taskName?: string | undefined,
+}
+
 export const TodoList = ({ nTasks }: TodoListProps) => {
   const [showAddTask, setShowAddTask] = useState(false);
+  const [activeTaskInfo, setActiveTaskInfo] = useState<TaskInfo>({});
+  const [showAddJob, setShowAddJob] = useState(false);
 
   const contracts = useMemo(() => range(0, nTasks).map(i => ({
     addressOrName: address,
@@ -29,7 +37,13 @@ export const TodoList = ({ nTasks }: TodoListProps) => {
     return <Loading />;
   }
 
-  
+  const onAddJob = (taskIdx: number) => {
+    setShowAddJob(true);
+    setActiveTaskInfo({
+      taskIdx,
+      taskName: data![taskIdx].name,
+    });
+  }
 
   return (
     <div className="flex flex-col mt-12 items-center">
@@ -39,13 +53,23 @@ export const TodoList = ({ nTasks }: TodoListProps) => {
             <th className="border text-left py-1 px-2">Task name</th>
             <th className="border text-left py-1 px-2">Description</th>
             <th className="border text-left py-1 px-2">Bounty</th>
+            <th className="border text-left py-1 px-2">Owner</th>
           </tr>
         </thead>
         <tbody>
           {data!.map((task, idx) => <tr key={idx}>
-              <td className="border py-1 px-2">{task.name}</td>
+              <td className="border py-1 px-2">
+                {task.name}
+                {task.n_submissions.gt(0) && <>
+                  <br />
+                  <a href="#" className="text-xs text-blue-400">{task.n_submissions.toNumber()} submissions</a>
+                </>}
+                <br />
+                <a href="#" onClick={() => onAddJob(idx)} className="text-xs text-blue-400">submit job</a>
+              </td>
               <td className="border py-1 px-2">{task.description}</td>
               <td className="border py-1 px-2">{task.bounty.toNumber()}</td>
+              <td className="border py-1 px-2">{task.owner.slice(0, 4)}...{task.owner.slice(-4)}</td>
             </tr>)}
         </tbody>
       </table>
@@ -62,6 +86,12 @@ export const TodoList = ({ nTasks }: TodoListProps) => {
       <AddTask
           open={showAddTask}
           setOpen={setShowAddTask}
+        />
+      <AddJob
+          open={showAddJob}
+          setOpen={setShowAddJob}
+          taskId={activeTaskInfo.taskIdx}
+          taskName={activeTaskInfo.taskName}
         />
     </div>
   );
